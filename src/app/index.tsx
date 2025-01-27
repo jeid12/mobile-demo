451a8a818480d983d05e70a2070ce36a92b35afb
jeid12 <niyokwizerajd123@gmail.com>
2025-01-27 09:42:29 +0200

    Started mobile development in React Native

---
+++ src/app/index.tsx
@@ -0,0 +1,12 @@
+ import { View, Text } from 'react-native'
+ import React from 'react'
+ 
+ const index = () => {
+   return (
+     <View>
+       <Text>My name Is Bro</Text>
+     </View>
+   )
+ }
+ 
+ export default index
